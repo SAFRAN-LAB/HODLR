8bf8ed92c82a2825e697eb9de0e0467a36bd28c5
Dan F-M <danfm@nyu.edu>
2014-02-02 17:38:22 -0500

    Python installation and usage instructions

--- python/README.rst
+++ python/README.rst
@@ -6,14 +6,51 @@ This is a light Python wrapper around the HODLR solver.
 Installation
 ------------
 
+In the ``python`` directory (the location of this file), run
 
+::
+
+    python setup.py install
+
+or, if you've installed Eigen in a custom location, run
+
+::
+
+    python setup.py build_ext --include-dirs=/path/to/eigen3
+    python setup.py install
 
 Usage
 -----
 
+This module exposes one class ``HODLR`` to perform all the computations. The
+basic usage will look like:
 
+::
+
+    import hodlr
+    import numpy as np
+
+    def matrix(i, j):
+        return np.exp(-0.5 * (i - j) ** 2)
+
+    # Set up the system.
+    N = 5000
+    diag = np.random.randn(N)
+    solver = hodlr.HODLR(matrix, diag)
+
+    # Build the true solution for testing purposes.
+    x = np.random.randn(N)
+    b = solver.matrix_product(x)
+
+    # Solve the system.
+    xsol = solver.solve(b)
+    print("L1 error: {0}".format(np.mean(np.abs(x - xsol))))
+
+    # Get the log-determinant.
+    print("Log-derterminant".format(solver.logdet()))
 
 Authors
 -------
 
-The solver is developed by Sivaram Ambikasaran and these Python bindings are developed by Daniel Foreman-Mackey.
+The solver is developed by Sivaram Ambikasaran and these Python bindings are
+developed by Daniel Foreman-Mackey.
--- python/setup.py
+++ python/setup.py
@@ -18,35 +18,41 @@ except ImportError:
 import numpy
 
 include_dirs = [
-    numpy.get_include(),
-    "${CMAKE_SOURCE_DIR}/header",
-    "${CMAKE_CURRENT_SOURCE_DIR}/hodlr",
-] + filter(lambda p: len(p), "${EIGEN_INCLUDE_DIRS}".split(";"))
+    numpy.get_include(), "../header", "hodlr",
+
+    # Standard Eigen includes. Provide custom paths with the
+    # `--include-dirs=/path/to/eigen3` flag when running `build_ext`.
+    "/usr/local/include/eigen3",
+    "/usr/local/homebrew/include/eigen3",
+    "/opt/local/var/macports/software/eigen3",
+    "/opt/local/include/eigen3",
+    "/usr/include/eigen3",
+]
 
 library_dirs = []
 libraries = ["m"]
-sources = [
-    "${CMAKE_CURRENT_SOURCE_DIR}/hodlr/_hodlr.cpp",
-]
+sources = ["hodlr/_hodlr.cpp", ]
+extra_compile_args = ["-ffast-math"]
 
 ext = Extension("hodlr._hodlr",
                 sources=sources,
                 library_dirs=library_dirs,
                 libraries=libraries,
                 runtime_library_dirs=library_dirs,
-                include_dirs=include_dirs)
+                include_dirs=include_dirs,
+                extra_compile_args=extra_compile_args)
 
 setup(
     name="hodlr",
-    version="${HODLR_VERSION}",
+    version="0.3.14",
     author="Daniel Foreman-Mackey",
     author_email="danfm@nyu.edu",
     url="https://github.com/sivaramambikasaran/HODLR_Solver",
     packages=["hodlr"],
     ext_modules=[ext],
     description="HODLR Solver",
-    long_description=open("${CMAKE_CURRENT_SOURCE_DIR}/README.rst").read(),
-    package_dir={"": "${CMAKE_CURRENT_SOURCE_DIR}"},
+    long_description=open("README.rst").read(),
+    install_requires=["numpy"],
     classifiers=[
         # "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
